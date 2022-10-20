5d7f45b49b8cac482c68a2c2ecf7d11685ce69cd
prageethmilan <prageethmilan1999@gmail.com>
2022-10-20 22:29:02 +0530

    Initial Commit

---
+++ src/main/java/com/example/springsecurity/User.java
@@ -0,0 +1,19 @@
+package com.example.springsecurity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    private Integer id;
+    private String username;
+    private String password;
+}
